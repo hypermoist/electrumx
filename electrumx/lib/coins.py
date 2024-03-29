bd14f05ca2477b692365b2915f8c68dbf06959ed
Ariel Marshall-Smith <jacobms109@gmail.com>
2024-03-30 00:11:57 +1300

    Hemis Addition Test

--- electrumx/lib/coins.py
+++ electrumx/lib/coins.py
@@ -4236,7 +4236,7 @@ class Hemis(Coin):
     RPC_PORT = 49165
     REORG_LIMIT = 100
     EXPANDED_HEADER = 112
-    SAPLING_START_HEIGHT = 503
+    SAPLING_START_HEIGHT = 501
     BLOCK_VERSION = 11
 
     @classmethod
