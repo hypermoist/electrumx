0f972b10aab244c6fc6886b5f3d00b0f1d17cc2b
Ariel Marshall-Smith <jacobms109@gmail.com>
2024-03-30 23:58:44 +1300

    Hemis...

--- electrumx/server/block_processor.py
+++ electrumx/server/block_processor.py
@@ -84,7 +84,6 @@ class Prefetcher:
         self.blocks = []
         self.cache_size = 0
         self.refill_event.set()
-        print(f'prefetched blocks: {blocks[0]}')
         return blocks
 
     async def reset_height(self, height):
@@ -236,10 +235,11 @@ class BlockProcessor:
         #print("First Header (hex):", header_hex)
         hprevs = [self.coin.header_prevhash(h) for h in headers]
         chain = [self.tip] + [self.coin.header_hash(h) for h in headers[:-1]]
+        print(f'Block {chain[0]}: {chain[-1]}')
         #print("Chain:", [hash.hex() for hash in chain])
         # Print the block hashes for the first 3 blocks
-        for i in range(min(3, len(chain))):
-            print(f'Block {i+1} hash: {chain[i].hex()}')
+    #    for i in range(min(3, len(chain))):
+    #        print(f'Block {i+1} hash: {chain[i].hex()}')
         if hprevs == chain:
             start = time.monotonic()
             await self.run_in_thread_with_lock(self.advance_blocks, blocks)
