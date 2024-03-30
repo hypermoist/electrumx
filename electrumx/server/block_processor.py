8244be175b381e0827f5fa5487fd18edeac3cb9b
Ariel Marshall-Smith <jacobms109@gmail.com>
2024-03-30 22:36:12 +1300

    Hemis...

--- electrumx/server/block_processor.py
+++ electrumx/server/block_processor.py
@@ -9,7 +9,6 @@
 '''Block prefetcher and chain processor.'''
 
 
-import binascii
 import asyncio
 import time
 from typing import Sequence, Tuple, List, Callable, Optional, TYPE_CHECKING, Type
@@ -233,7 +232,8 @@ class BlockProcessor:
                   for n, raw_block in enumerate(raw_blocks)]
         headers = [block.header for block in blocks]
         hprevs = [self.coin.header_prevhash(h) for h in headers]
-        chain = [self.tip] + [binascii.hexlify(self.coin.header_hash(h)).decode('utf-8') for h in headers[:-1]]
+        chain = [self.tip] + [self.coin.header_hash(h) for h in headers[:-1]]
+        print("Chain:", [hash.hex() for hash in chain])
         # Print the block hashes for the first 3 blocks
         for i in range(min(3, len(chain))):
             print(f'Block {i+1} hash: {chain[i].hex()}')
